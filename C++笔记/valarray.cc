9a94098a32c7c1533c6276c10343b724b589a14d
sekka16 <729840088@qq.com>
2024-06-05 17:44:57 +0800

    simple valarray

--- C++笔记/valarray.cc
+++ C++笔记/valarray.cc
@@ -1,29 +1,67 @@
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
-#include <valarray>
+#include <new>
+#include <type_traits>
 
 constexpr int VECTOR_LENGTH = 10;
 
 template <typename T> class vector {
-public:
-  typedef T value_type;
-  typedef T *iterator;
+  static_assert(std::is_same<T, int>::value || std::is_same<T, float>::value,
+                "Invalid type for vector, must be int or float");
 
 private:
+  typedef T value_type;
+
+public:
   value_type *_data;
   size_t _size;
   size_t _capacity;
 
 public:
   // 构造函数
-  vector() : _data(NULL), _size(0), _capacity(VECTOR_LENGTH) {}
+  vector() : _data(NULL), _size(0), _capacity(VECTOR_LENGTH) {
+    _data = new (std::nothrow) value_type[_capacity];
 
-  vector(size_t _size) : _data(NULL), _size(_size), _capacity(VECTOR_LENGTH) {}
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+  }
+
+  explicit vector(size_t _size)
+      : _data(NULL), _size(_size), _capacity(VECTOR_LENGTH) {
+    while (_size > _capacity) {
+      _capacity *= 2;
+    }
+
+    _data = new (std::nothrow) value_type[_capacity];
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+  }
+
+  vector(std::initializer_list<value_type> values)
+      : _size(values.size()), _capacity(values.size()) {
+    _data = new (std::nothrow) value_type[_capacity];
+
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+
+    std::copy(values.begin(), values.end(), _data);
+  }
 
   // 拷贝构造函数(深拷贝)
   vector(const vector &vec) {
     _size = vec._size;
     _capacity = vec._capacity;
-    _data = new value_type[_capacity];
+
+    _data = new (std::nothrow) value_type[_capacity];
+    if (!_data) {
+      throw std::bad_alloc();
+    }
 
     std::copy(vec._data, vec._data + vec._size, _data);
   }
@@ -36,51 +74,180 @@ public:
     _capacity = 0;
   }
 
-  // 运算符重载
+  // =
   vector &operator=(const vector &vec) {
     if (this == &vec)
       return *this;
 
-    value_type *tmp = new value_type[vec._capacity];
-    std::copy(vec._data, vec._data + vec._size, tmp);
+    vector tmp(vec);
 
     delete[] _data;
+    _data = tmp._data;
+    _size = tmp._size;
+    _capacity = tmp._capacity;
 
-    _data = tmp;
-    _size = vec._size;
-    _capacity = vec._capacity;
+    tmp._data = NULL;
+    tmp._size = 0;
+    tmp._capacity = 0;
 
     return *this;
   }
 
-  void operator+(const vector &vec) {
+  // +
+  vector operator+(const vector &vec) {
     size_t common_size = std::min(_size, vec._size);
+    vector result(common_size);
 
-    std::transform(_data, _data + common_size, vec._data, _data,
+    std::transform(_data, _data + common_size, vec._data, result._data,
                    std::plus<value_type>());
+
+    return result;
   }
 
-  void operator-(const vector &vec) {
-    size_t common_size = std::min(_size, vec._size);
+  // // -(minus)
+  // vector operator-(const vector &vec) {
+  //   size_t common_size = std::min(_size, vec._size);
+  //   vector result(common_size);
+  //
+  //   std::transform(_data, _data + common_size, vec._data, result._data,
+  //                  std::minus<value_type>());
+  //   return result;
+  // }
 
-    std::transform(_data, _data + common_size, vec._data, _data,
-                   std::minus<value_type>());
+  // -(neg)
+  vector operator-() const {
+    vector result(_size);
+    std::transform(_data, _data + _size, result._data,
+                   std::negate<value_type>());
+    return result;
   }
 
-  void operator-() {
-    std::transform(_data, _data + _size, _data, std::negate<value_type>());
+  vector abs() {
+    vector result(_size);
+    std::transform(_data, _data + _size, result._data,
+                   [](value_type val) { return std::abs(val); });
+    return result;
   }
 
-  // operator+, -, -(neg), =
-  // operator=
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const vector<value_type> &vec) {
+    os << "[";
+    for (size_t i = 0; i < vec._size; ++i) {
+      os << vec._data[i];
+      if (i < vec._size - 1) {
+        os << ", ";
+      }
+    }
+    os << "]";
+    return os;
+  }
 };
+
+// +
+template <typename T1, typename T2>
+auto operator+(const vector<T1> &vec1,
+               const vector<T2> &vec2) -> vector<decltype(T1() + T2())> {
+  using VecType = decltype(T1() + T2());
+  size_t common_size = std::min(vec1._size, vec2._size);
+  vector<VecType> result(common_size);
+
+  std::transform(vec1._data, vec1._data + common_size, vec2._data, result._data,
+                 std::plus<VecType>());
+
+  return result;
+}
+// -
+template <typename T1, typename T2>
+auto operator-(const vector<T1> &vec1,
+               const vector<T2> &vec2) -> vector<decltype(T1() + T2())> {
+  using VecType = decltype(T1() + T2());
+  size_t common_size = std::min(vec1._size, vec2._size);
+  vector<VecType> result(common_size);
+
+  std::transform(vec1._data, vec1._data + common_size, vec2._data, result._data,
+                 std::minus<VecType>());
+
+  return result;
+}
+
+template <typename T1, typename T2>
+vector<T1> operator+(const T2 &scalar, const vector<T1> &vec) {
+  using VecType = typename std::conditional<
+      std::is_same<decltype(T1() + T2()), double>::value, float,
+      decltype(T1() + T2())>::type;
+  vector<VecType> result(vec._size);
+
+  std::transform(vec._data, vec._data + vec._size, result._data,
+                 [=](VecType val) { return val + scalar; });
+
+  return result;
+}
+
+template <typename T1, typename T2>
+vector<T1> operator+(const vector<T1> &vec, const T2 &scalar) {
+  using VecType = typename std::conditional<
+      std::is_same<decltype(T1() + T2()), double>::value, float,
+      decltype(T1() + T2())>::type;
+  vector<VecType> result(vec._size);
+
+  std::transform(vec._data, vec._data + vec._size, result._data,
+                 [=](VecType val) { return val + scalar; });
+
+  return result;
+}
+
+template <typename T1, typename T2>
+vector<T1> operator-(const T2 &scalar, const vector<T1> &vec) {
+  using VecType = typename std::conditional<
+      std::is_same<decltype(T1() + T2()), double>::value, float,
+      decltype(T1() + T2())>::type;
+  vector<VecType> result(vec._size);
+
+  std::transform(vec._data, vec._data + vec._size, result._data,
+                 [=](VecType val) { return scalar - val; });
+
+  return result;
+}
+
+template <typename T1, typename T2>
+vector<T1> operator-(const vector<T1> &vec, const T2 &scalar) {
+  using VecType = typename std::conditional<
+      std::is_same<decltype(T1() + T2()), double>::value, float,
+      decltype(T1() + T2())>::type;
+  vector<VecType> result(vec._size);
+
+  std::transform(vec._data, vec._data + vec._size, result._data,
+                 [=](VecType val) { return val - scalar; });
+
+  return result;
+}
 
 // vector abs(vector) {}
 
 int main() {
   vector<int> x{1, 2, 3, 4, 5};
-  vector<int> y;
+  vector<float> y{3.0f, 5.2f, -1.0f, 0.0f, 2.0f};
+  // vector<double> y;
 
-  vector<int> z;
+  vector<int> x1, x2, x3, x4, x5;
+  x1 = x + x;
+  x2 = x - x;
+  x3 = -x;
+  x4 = x3.abs();
+  x5 = x1 + 2;
+
+  vector<float> z;
+  // ..
   z = -(x - y + 2) - 4.5;
+
+  std::cout << x1 << std::endl;
+  std::cout << x2 << std::endl;
+  std::cout << x3 << std::endl;
+  std::cout << x4 << std::endl;
+  std::cout << x5 << std::endl;
+  std::cout << x + y << std::endl;
+
+  std::cout << z << std::endl;
+
+  return 0;
 }
--- C++笔记/valarray.md
+++ C++笔记/valarray.md
@@ -5,10 +5,10 @@
 ## vector的成员变量
 
 ```cpp
-public:
-    typedef T value_type;  // 这个并不是成员变量，主要时为了增强可读性
-    typedef T* iterator;   // 其时就是重新起别名
 private:
+    typedef t value_type;  // 这个并不是成员变量，主要时为了增强可读性
+
+public:
     value_type* _data;
     size_t _size;
     size_t _capacity;
@@ -20,39 +20,212 @@ private:
 ## 构造函数
 
 ```cpp
-// 构造函数
-vector():_data(NULL),_size(0),_capacity(0){}
+  // 构造函数
+  vector() : _data(null), _size(0), _capacity(vector_length) {
+    _data = new (std::nothrow) value_type[_capacity];
 
-// 析构函数
-~vector(){
-    delete [] _data;
-    _data = NULL;
-    _size = 0;
-    _capacity = 0;
-}
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+  }
 
-// 拷贝构造函数(深拷贝)
-vector(const vector& vec){
+  explicit vector(size_t _size)
+      : _data(null), _size(_size), _capacity(vector_length) {
+    while (_size > _capacity) {
+      _capacity *= 2;
+    }
+
+    _data = new (std::nothrow) value_type[_capacity];
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+  }
+
+  vector(std::initializer_list<value_type> values)
+      : _size(values.size()), _capacity(values.size()) {
+    _data = new (std::nothrow) value_type[_capacity];
+
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+
+    std::copy(values.begin(), values.end(), _data);
+  }
+
+  // 拷贝构造函数(深拷贝)
+  vector(const vector &vec) {
     _size = vec._size;
     _capacity = vec._capacity;
-    _data = new value_type[_capacity];
-    for(int i=0;i<_size;++i){
-        _data[i] = vec._data[i];
+
+    _data = new (std::nothrow) value_type[_capacity];
+    if (!_data) {
+      throw std::bad_alloc();
     }
-}
+
+    std::copy(vec._data, vec._data + vec._size, _data);
+  }
 ```
 
 > 浅拷贝是拷贝了对象的引用，当原对象发生变化的时候，拷贝对象也跟着变化；
 深拷贝是另外申请了一块内存，内容和原对象一样，更改原对象，拷贝对象不会发生变化。 
 
-## this指针
+## 运算符重载
 
-在 C++ 中，this 指针是一个特殊的指针，它指向当前对象的实例。
-在 C++ 中，每一个对象都能通过 this 指针来访问自己的地址。
-this是一个隐藏的指针，可以在类的成员函数中使用，它可以用来指向调用对象。
-当一个对象的成员函数被调用时，编译器会隐式地传递该对象的地址作为 this 指针。
-友元函数没有 this 指针，因为友元不是类的成员，只有成员函数才有 this 指针。
+### operator=
 
 ```cpp
+  vector &operator=(const vector &vec) {
+    if (this == &vec)
+      return *this;
 
+    vector tmp(vec);
+
+    delete[] _data;
+    _data = tmp._data;
+    _size = tmp._size;
+    _capacity = tmp._capacity;
+
+    tmp._data = null;
+    tmp._size = 0;
+    tmp._capacity = 0;
+
+    return *this;
+  }
 ```
+
+> 在 c++ 中，this 指针是一个特殊的指针，它指向当前对象的实例。
+友元函数没有 this 指针，因为友元不是类的成员，只有成员函数才有 this 指针。
+
+### operator+
+
+本例中实现的`vector`限定数据类型为`float`,`int`，
+在处理不同类型的`vector`相加时定义为：转化成float
+在处理`vector`和标量相加时定义为：推断结果类型，如果是double转化成float，否则保持原类型。
+
+```cpp
+// 向量+向量
+template <typename t1, typename t2>
+auto operator+(const vector<t1> &vec1,
+               const vector<t2> &vec2) -> vector<decltype(t1() + t2())> {
+  using vectype = decltype(t1() + t2());
+  size_t common_size = std::min(vec1._size, vec2._size);
+  vector<vectype> result(common_size);
+
+  std::transform(vec1._data, vec1._data + common_size, vec2._data, result._data,
+                 std::plus<vectype>());
+
+  return result;
+}
+
+// 标量+向量
+template <typename t1, typename t2>
+vector<t1> operator+(const t2 &scalar, const vector<t1> &vec) {
+  using vectype = typename std::conditional<
+      std::is_same<decltype(t1() + t2()), double>::value, float,
+      decltype(t1() + t2())>::type;
+  vector<vectype> result(vec._size);
+
+  std::transform(vec._data, vec._data + vec._size, result._data,
+                 [=](vectype val) { return val + scalar; });
+
+  return result;
+}
+
+// 向量+标量
+template <typename t1, typename t2>
+vector<t1> operator+(const vector<t1> &vec, const t2 &scalar) {
+  using vectype = typename std::conditional<
+      std::is_same<decltype(t1() + t2()), double>::value, float,
+      decltype(t1() + t2())>::type;
+  vector<vectype> result(vec._size);
+
+  std::transform(vec._data, vec._data + vec._size, result._data,
+                 [=](vectype val) { return val + scalar; });
+
+  return result;
+}
+```
+
+### 问题
+
+```cpp
+  // 假如类中定义
+  vector operator+(const vector &vec) {
+    size_t common_size = std::min(_size, vec._size);
+    vector result(common_size);
+
+    std::transform(_data, _data + common_size, vec._data, result._data,
+                   std::plus<value_type>());
+    return result;
+  }
+
+  // 类外定义
+  template <typename t1, typename t2>
+  vector<t1> operator+(const vector<t1> &vec, const t2 &scalar) {
+    using vectype = typename std::conditional<
+        std::is_same<decltype(t1() + t2()), double>::value, float,
+        decltype(t1() + t2())>::type;
+    vector<vectype> result(vec._size);
+
+    std::transform(vec._data, vec._data + vec._size, result._data,
+                   [=](vectype val) { return val + scalar; });
+
+    return result;
+  }
+
+  // 并且你的类中有这样一个构造函数
+  vector(size_t _size)
+      : _data(NULL), _size(_size), _capacity(VECTOR_LENGTH) {
+    while (_size > _capacity) {
+      _capacity *= 2;
+    }
+
+    _data = new (std::nothrow) value_type[_capacity];
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+  }
+```
+
+这样的话在执行一些语句时可能会出现警告
+
+例如：
+```cpp
+x5 = x1 + 2;
+```
+
+```bash
+valarray.cc:236:13: warning: ISO C++ says that these are ambiguous, even though 
+the worst conversion for the first is better than the worst conversion for the s
+econd:
+  236 |   x5 = x1 + 2;
+      |             ^
+valarray.cc:186:12: note: candidate 1: ‘vector<T1> operator+(const vector<T1>&, 
+const T2&) [with T1 = int; T2 = int]’
+  186 | vector<T1> operator+(const vector<T1> &vec, const T2 &scalar) {
+      |            ^~~~~~~~
+valarray.cc:96:10: note: candidate 2: ‘vector<T> vector<T>::operator+(const vect
+or<T>&) [with T = int]’
+   96 |   vector operator+(const vector &vec) {
+      |          ^~~~~~~~
+```
+
+造成这种警告的原因是：2被解释成构造函数的参数，编译器可能会尝试通过2这个参数创建一个`vector`，而不是将其解释为一个标量。因此会产生二义性。
+
+**解决方法**：增加`explicit`关键字，使参数不能被进行隐式转化。
+
+```cpp
+  explicit vector(size_t _size)
+      : _data(NULL), _size(_size), _capacity(VECTOR_LENGTH) {
+    while (_size > _capacity) {
+      _capacity *= 2;
+    }
+
+    _data = new (std::nothrow) value_type[_capacity];
+    if (!_data) {
+      throw std::bad_alloc();
+    }
+  }
+```
+
+> 在C++中，explicit关键字用于修饰单参数构造函数，防止编译器进行隐式类型转换。它的作用是禁止编译器通过隐式转换调用该构造函数。
